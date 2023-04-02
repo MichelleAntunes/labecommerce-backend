4c9417e938e3e8d2f1410403e4999adbebee4223
MichelleAntunes <miichelleantuness@gmail.com>
2023-04-02 16:52:49 +0200

    aprofundamento sql pt 2

--- src/dataBase/labecommerce.sql
+++ src/dataBase/labecommerce.sql
@@ -4,14 +4,27 @@ CREATE TABLE users ( id TEXT PRIMARY KEY UNIQUE NOT NULL,
  email TEXT UNIQUE NOT NULL,   
  password TEXT NOT NULL
  );
-
- SELECT * FROM users;
+--Get All Users
+ SELECT * FROM users
+ ORDER BY email ASC;
 
  INSERT INTO users(id, email, password)
-VALUES ("001", "usuario_numero1@email.com", "01C02");
+VALUES ("001", "usuario_numero1@email.com", "01C02"),
+("002", "usuario_numero2@email.com", "0jk00"),
+("003", "usuario_numero3@email.com", "asd142");
 
-INSERT INTO users(id, email, password)
-VALUES ("002", "usuario_numero2@email.com", "0jk00");
+INSERT INTO users (id, email, password)
+VALUES ("004", "usuario_numero4@email.com", "01478");
 
-INSERT INTO users(id, email, password)
-VALUES ("003", "usuario_numero3@email.com", "asd142");
+--Get User by id
+SELECT * FROM users
+WHERE id = "001";
+
+-- Delete User by id
+DELETE FROM users
+WHERE id = "001";
+
+-- Edit User by id
+UPDATE users
+SET id = "Bananinha"
+WHERE id = "002";
